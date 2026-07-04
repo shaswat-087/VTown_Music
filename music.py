2095fc543c5cc1ae360ba619782f9b3a2422bb97
Shaswat <madhavbanerjee316@gmail.com>
2026-07-04 22:01:33 +0530

    Update music.py

--- music.py
+++ music.py
@@ -5,60 +5,94 @@ df=pd.read_csv('Vtown.csv')
 info=df[['track_id','title','artist','actor']]
 actor_counts = df['actor'].value_counts()
 artist_counts = df['artist'].value_counts()
-
+play_counts={}
 
 df['actor_weight'] = df['actor'].map(actor_counts).fillna(0) / 100
 df['artist_weight'] = df['artist'].map(artist_counts).fillna(0) / 100
 feature=df[['actor_weight','artist_weight','energy','danceability','valence']]
 x=df['energy'].median()
 y=df['valence'].median()
+z=df['danceability'].median()
 arr=feature.to_numpy()
 conditions=[
-      (df['energy']>=x) & (df['valence']>=y),
-      (df['energy']<=x) & (df['valence']>=y),
-      (df['energy']>=x) & (df['valence']<=y),
-      (df['energy']<=x) & (df['valence']<=y)
+      (df['energy']>=x) & (df['valence']>=y) & (df['danceability']>=z),
+      (df['energy']>=x) & (df['valence']>=y) & (df['danceability']<=z),
+
+      (df['energy']<=x) & (df['valence']>=y) &(df['danceability']>=z),
+      (df['energy']<=x) & (df['valence']>=y) &(df['danceability']<=z),
+
+      (df['energy']>=x) & (df['valence']<=y) &(df['danceability']>=z),
+      (df['energy']>=x) & (df['valence']<=y) &(df['danceability']<=z),
+  
+      (df['energy']<=x) & (df['valence']<=y) & (df['danceability']>=z),
+      (df['energy']<=x) & (df['valence']<=y) & (df['danceability']<=z)
 ]
 
 quadrant_labels=[
       'Party',
-      'Chill',
-      'Gym & Workout',
-      'Sad Songs'
+      'Feel Good',
+      'Romance',
+      'Relax',
+      'Work Out',
+      'Energize',
+      'Focus',
+      'Sad'
+  
+      
 ]
-
-df['playlist']=np.select(conditions,quadrant_labels,default="Mix")
-gym=df[df['playlist']=='Gym & Workout']
-gym_sorted=gym.sort_values(by='energy',ascending=False)
-print("Your gym & workout mix \n",gym_sorted['title'])
-
-party=df[df['playlist']=='Party']
-print("Your party mix \n",party['title'])
  
+df['playlist']=np.select(conditions,quadrant_labels,default="Mix")
 
 content=df['title'].str.strip().str.lower()
+user=np.array([0.2,0.2,0.5,0.5,0.5])
+N=0
+df['distance'] = np.linalg.norm(arr - user, axis=1)
+top_10 = np.argsort(df['distance'].to_numpy())[:10]
 while True:
-     choice=input("Search a song you want to listen . Press 5 to exit \n").strip().lower()
-     if choice=='5':
+       menu_choice=int(input("Press 1 to Search \n 2 to View Feed \n 3 to View Playlist \n 4 to View Repeat Tracks \n 5 to exit"))
+
+       if menu_choice==5:
         print("Successful Exit")
         break
-     found=False
-     for i,song in enumerate(content):
-          if song==choice:
+       if menu_choice==1:
+         choice=input("Search a song you want to listen . \n").strip().lower()
+         found=False
+         for i,song in enumerate(content):
+            if song==choice:
                  print(f"\nFound {choice} on row {i}")
                  found=True
                  index=i
+                 N=N+1
+                 a=1/N
+                 track_id=df.iloc[i]['track_id']
+                 user=((1-a)*user)+(a*arr[i])
+                 play_counts[track_id]=play_counts.get(track_id,0)+1
+                 break
                  
-     if found:
-            target=arr[index]   
-     if not found:
-            print(" Not found. Try Searching Again ")
+         if found:
+            target=user
+            distance=np.linalg.norm(arr-target,axis=1)
+            df['distance'] = distance
+            sorted=np.argsort(distance)
+            top_10=sorted[1:11]
+         else:
+           print("Song Not Found. Try Searching Again .")
 
+       if menu_choice==2:
+            print("Here are top 10 songs you would like next")
+            print(df.iloc[top_10]['title'])
+       if menu_choice==3: 
+            for label in quadrant_labels:
+                 sorted_playlist=df[df['playlist']==label].sort_values(by='distance')
 
-
-     distance=np.linalg.norm(arr-target,axis=1)
-     sorted=np.argsort(distance)
-     top_10=sorted[1:11]
-     print("Here are top 10 songs you would like next")
-     print(df.iloc[top_10]['title'])
-
+                 if not sorted_playlist.empty:
+                    print(f"\nYour {label} Mix:")
+                    print(sorted_playlist['title'].to_string(index=True))
+       if menu_choice==4:
+            for track_id,count in play_counts.items():
+                if  count>5:
+                       print("Listen Again ")
+                       print(df[df['track_id']==track_id]['title'])
+       
+       
+    
